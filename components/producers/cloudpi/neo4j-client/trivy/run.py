ac7867bd5d1584dd6d0ecce15086b90417c2b1fe
Alberto Rodriguez <alberto.rfernandez4@gmail.com>
2024-10-21 16:43:07 +0200

    feat: add demo

--- components/producers/cloudpi/neo4j-client/trivy/run.py
+++ components/producers/cloudpi/neo4j-client/trivy/run.py
@@ -1,12 +1,13 @@
 import json
 import subprocess
+import os
 
 def scan_ebs(snap_name):
     try:
         # Run Trivy scan command for the specified ebs
         # trivy vm --scanners vuln ebs:snap-02f3d4e008898f8d0 --aws-region eu-west-1
         result = subprocess.run(
-            ['trivy', 'vm', '--scanners', 'vuln', '--format', 'json', f"ebs:{snap_name}"],
+            ['trivy', 'vm', '--scanners', 'vuln', '--format', 'json', '--severity', os.GetEnv("SEVERITY"), f"ebs:{snap_name}"],
             capture_output=True,
             text=True,
             check=True
